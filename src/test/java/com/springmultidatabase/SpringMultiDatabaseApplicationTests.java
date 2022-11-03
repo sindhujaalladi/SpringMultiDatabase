3f588557b554600a212ed82b57e163d92a52ea7f
sindhujaalladi <sindhu.alladi1993@gmail.com>
2022-11-03 12:17:33 -0500

    inital commit

---
+++ src/test/java/com/springmultidatabase/SpringMultiDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmultidatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
